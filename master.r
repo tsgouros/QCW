ca9dd29d2c17ca0e61be495e051e92080604c7db
tsgouros <tsgouros@gmail.com>
2024-10-01 18:03:23 -0400

    added master and read-county

---
+++ master.r
@@ -0,0 +1,105 @@
+## A master file, describing the steps of our processing.
+##
+library(tidyverse)
+library(anytime)
+library(lubridate)
+library(readxl)
+library(grid)
+library(png)
+library(ggplot2)
+library(ggrepel)
+
+## These are the time-consuming tasks, with flags to run them or not.
+
+## Getting the more recent billing data.
+recompile <- FALSE;
+## Getting the data from the original water billing data...
+recompile.pre.2023 <- FALSE;
+## ... and the county data.
+recompile.county <- FALSE;
+## Doing the sampling and preliminary processing of the big pile of data.
+prelim <- FALSE;
+## Creating the model with account data and growth projections
+incorporateGrowth <- FALSE;
+## Output graphics
+saveGraphs <- FALSE;
+
+## Output graphics
+saveGraphs <- FALSE;
+
+## Not time consuming, but this data is good for debugging.
+cleanup <- FALSE;
+
+## To establish consistency in creating the time base from year and
+## month data.
+convertDateToInteger <- function(year, month) {
+    return((year - 2017) * 12 + (month - 6));
+}
+
+## *** GATHER DATA
+##
+##
+## First, incorporate the data from QC water.
+if (recompile) source("intake.r");
+
+## This brings the pre-2023 data into a tibble called 'big'.
+if (recompile.pre.2023) source("intake-2.r");
+##
+## Read the property data from Maricopa and Pinal counties.
+if (recompile.county) source("~/osa/20/queenscreek/read-county-data.r",
+                             chdir=TRUE);
+## This creates a big array called 'prop'.
+
+waterTable <- billTable %>%
+    ## April had stolen some bills from March in 2023. Maybe May, too? 
+    mutate(billMonth=ifelse(billDate>ymd("2023-03-01")&
+                            billDate<ymd("2023-04-12"),
+                            3,
+                     ifelse(billDate>=ymd("2023-04-12")&
+                            billDate<ymd("2023-05-09"),
+                            4,
+                            billMonth))) %>% 
+    right_join(readingTable %>%
+              select(billNumber, service, meter, currentReading,
+                     previousReading, consumption,
+                     readingDate, readingMonth, readStatus,
+                     previousReadingDate, previousReadingMonth) %>%
+              ## there are records where the following is not true, seem mostly
+              ## to be from 2/2023.
+              filter(consumption == (currentReading - previousReading)),
+              by="billNumber") %>%
+    left_join(customerTable %>% group_by(customer) %>%
+              dplyr::summarize(revenueClass=first(revenueClass)),
+              by="customer") %>%
+    mutate(waterUseSegment=ifelse(service=="Irrigation",
+                                  "Irrigation",
+                           ifelse(service=="Landscape",
+                           ifelse(revenueClass=="RECOVERED EFFLUENT",
+                                  "Recovered Effluent",
+                                  "Landscape"),
+                           ifelse(revenueClass=="SCHOOL/LARGE TURF 10+ ACRES" |
+                                  revenueClass=="SCHOOL",
+                                  "School",
+                           ifelse(service=="Rental Meters",
+                                  "Construction",
+                           ifelse(accountType=="RE" | accountType=="MF",
+                                  "Residential",
+                           ifelse(accountType=="MU",
+                                  "Government",
+                           ifelse(accountType=="CO",
+                                  "Commercial",
+                           ifelse(accountType=="TM",
+                                  "Construction",
+                                  "Unknown")))))))))
+
+## Bring in precipitation and temperature records
+source("rain.r")
+
+## Make a usage record with quarterly averages to match the seasonal
+## weather records. These are 3-month seasons, starting with July,
+## though perhaps we should test that.
+
+## Combine the big and waterTable usage records. Watch out, this one
+## can take a long time, depending on the flags set over there.
+if (prelim) source("~/osa/20/queenscreek/master-taz.r", chdir=TRUE);
+
---
+++ read-county-data.r
@@ -0,0 +1,389 @@
+library(anytime)
+library(tidyverse)
+library(lubridate)
+
+## From varhandle package
+## Function Description:
+##     A function to assess if a vector can be interpreted as numbers
+
+check.numeric <- function(v = NULL, na.rm = FALSE, only.integer = FALSE,
+                          exceptions=c(""), ignore.whitespace = TRUE){
+    #----[ checking the input ]----#
+    {
+        # if the only.integer is NOT a single TRUE or FALSE
+        if (!is.logical(only.integer) | length(only.integer) != 1) {
+            # complain
+            stop("The parameter \"only.integer\" should be either TRUE or FALSE.")
+        }
+
+        # if user has not defined the vector v
+        if (is.null(v)) {
+          # complain
+            stop("The parameter \"v\" is not defined. It can be character vector, numeric vector, factor vector or logical vector.")
+        # if user has defined but the class is NOT character or factor
+        }else if (!inherits(v, c("character", "factor"))) {
+            # if the class is NOT numeric or integer either
+            if (!inherits(v, c("numeric", "integer", "logical"))) {
+                # complain
+                stop("The parameter \"v\" can only be a character vector, numeric vector, factor vector or logical vector.")
+            # if the class is numeric or integer
+            }else{
+                # if user wants to specifically filter out non-integers, there
+                # is a chance that the vector contains some non-integer numbers
+                # so we should turn the vector to character and run the function
+                if(only.integer){
+                    # convert the vector to character
+                    v <- as.character(v)
+                }else{
+                    # since it is already a number
+                    return(rep(x = TRUE, length(v)))
+                }
+            }
+        }
+
+        # if the na.rm is NOT a single TRUE or FALSE
+        if (!is.logical(na.rm) | length(na.rm) != 1) {
+            # complain
+            stop("The parameter \"na.rm\" should be either TRUE or FALSE.")
+        }
+
+
+
+        # if the ignore.whitespace is NOT a single TRUE or FALSE
+        if (!is.logical(ignore.whitespace) | length(ignore.whitespace) != 1) {
+            # complain
+            stop("The parameter \"ignore.whitespace\" should be either TRUE or FALSE.")
+        }
+    }
+
+
+    #----[ pre-processing ]----#
+    {
+        # convert to character if it is vector
+        if (inherits(v, "factor")) {
+            # convert to character
+            v <- as.character(v)
+        }
+
+        # if user wants to ignore NAs
+        if (na.rm) {
+            # if it has some NAs
+            if (any(is.na(v))) {
+                # remove NAs
+                v <- v[-pin.na(v)]
+            }
+        }
+
+        # if user wants to ignore leading or tailing white space
+        if (ignore.whitespace) {
+            # substitute whitespaces in the begining and at the ending
+            # of each item in v
+            v <- gsub("^\\s+|\\s+$", "", v)
+        }
+    }
+
+
+    #----[ processing ]----#
+    {
+        # if user wants to only detect integers
+        if (only.integer) {
+            regexp_pattern <- "(^(-|\\+)?\\d+$)|(^(-|\\+)?(\\d*)e(-|\\+)?(\\d+)$)"
+        # if user wants to detect all numbers
+        }else{
+            #regexp_pattern <- "^(-|\\+)?\\d+(\\.?\\d+)?$"
+            regexp_pattern <- "(^(-|\\+)?((\\.?\\d+)|(\\d+\\.\\d+)|(\\d+\\.?))$)|(^(-|\\+)?((\\.?\\d+)|(\\d+\\.\\d+)|(\\d+\\.?))e(-|\\+)?(\\d+)$)"
+        }
+
+        # perform the regexp
+        output <- grepl(pattern = regexp_pattern, x = v)
+
+        # check for existance of exceptions
+        exception_index <- is.element(v, exceptions)
+        # if there are is exception detected
+        if (any(exception_index)) {
+            # turn their output value to TRUE
+            output[exception_index] <- TRUE
+        }
+
+        # if user wants to keep NA
+        if (!na.rm) {
+            # NAs are marked as FALSE by grepl and we replace it with TRUE instead
+            output[is.na(v)] <- TRUE
+        }
+
+
+        # return the result
+        return(output)
+    }
+}
+
+read.pinal.data <- function(fileName) {
+
+    cnames <- c("acct", "parcel", "parcelid", "accttype",
+                "propertytype", "impconditiontype", "impquality",
+                "implegalclass", "primarybltasdescription", "yearbuilt",
+                "physicalage", "adjustedyearbuilt", "effectiveage",
+                "totalimpsf", "residentialsf", "commercialsf", "condoimpsf",
+                "landlegalclass", "landnetacrecount", "taxareacode", "fcv",
+                "impvalue", "landvalue", "receptionno", "saledate", "saleprice",
+                "ownername1", "ownername2", "co", "mailingaddress", "ownercity",
+                "ownerstatecode", "ownerzipcode", "ownerprovince",
+                "ownerpostalcode", "ownercountry", "propertystreetno",
+                "propertypredirection", "propertystreetname",
+                "propertystreettype", "propertypostdirection",
+                "propertyunitname", "propertycity", "propertyzipcode",
+                "section", "township", "range", "status", "pool");
+
+    cclasses <- c("character",    "character",    "character",    "character",
+                  "character","character","character",
+                  "character","character","numeric",
+                  "numeric","numeric","numeric",
+                  "numeric","numeric","numeric","numeric",
+                  "character","numeric","character","numeric",
+                  "numeric","numeric","character","character","numeric",
+                  "character","character","character","character","character",
+                  "character","character","character",
+                  "character","character","character",
+                  "character","character",
+                  "character","character",
+                  "character","character","character",
+                  "character","character","character","character",
+                  "character");
+
+    test <- read.csv(fileName, sep=",", header=TRUE,
+                     col.names=cnames, colClasses=cclasses);
+
+    test$county <- rep("pinal", length(test$parcel));
+
+    convert.strings <- function(ins) {
+        outs <- c();
+        for (s in ins) {
+            outs <- c(outs, ifelse(is.na(as.numeric(s)),s,
+                                   as.character(as.numeric(s))));
+        }
+        return(factor(outs, levels=c("3", "4", "2", "1", "9", "5", "")));
+    }
+
+    ## Convert pool data to logical.
+    test$pool <- test$pool == "Yes";
+
+
+    ## Maricopa has pool area data, so add a dummy variable to match it.
+    test$poolarea <- rep(NA, length(test$parcel));
+    
+    return(tibble(parcel=test$parcel,
+                  type=test$propertytype,
+                  ##condition=test$impconditiontype,
+                  impvalue=test$impvalue,
+                  landvalue=test$landvalue,
+                  fcv=test$fcv,
+                  totalsf=ifelse(is.na(test$totalimpsf),
+                                 0, as.numeric(test$totalimpsf)),
+                  impclass=convert.strings(substring(test$implegalclass,1,2)),
+                  landclass=convert.strings(substring(test$landlegalclass,1,2)),
+                  area=test$landnetacrecount,
+                  address=gsub("^\\s+|\\s+$", "",
+                               gsub("  ", " ",
+                                    paste(test$propertystreetno,
+                                          test$propertypredirection,
+                                          test$propertystreetname,
+                                          test$propertystreettype,
+                                          test$propertypostdirection,
+                                          test$propertyunitname,
+                                          sep=" "))),
+                  section=factor(sapply(test$section,
+                                        function(x) { if (nchar(x)==1)
+                                                          paste0("0",x,sep="")
+                                                      else x},
+                                        simplify=TRUE)),
+                  township=factor(gsub("^0", "", test$township),
+                                  levels=c("2S", "3S", "1N")),
+                  range=(gsub("^0", "", test$range)),
+                  ownercity=test$ownercity,
+                  propertycity=test$propertycity,
+                  county=factor(test$county, levels=c("maricopa", "pinal")),
+                  poolarea=test$poolarea,
+                  pool=test$pool,
+                  yearbuilt=test$yearbuilt
+                  ));
+}
+
+pinal <- read.pinal.data("data/pinal/Account-Inventory-with-Land-Values-and-Pools-from-Imported-2020-List-Queen-Creek-Water-Board-5-24-2022.csv");
+pinal.new <- read.pinal.data("data/pinal/pinal-new-parcel-data.csv");
+
+pinal <- rbind(pinal, pinal.new);
+rm(pinal.new);
+
+
+## Given a list of parcel numbers, returns something that looks like
+## the pinal data, but only for the given parcels.
+read.maricopa.data <- function(desiredparcels) {
+
+    # returns string w/o leading or trailing whitespace
+    trim <- function(str) gsub("^\\s+|\\s+$", "", str);
+    trimz <- function(str) gsub("^0", "", trim(str));
+    trimdub <- function(str) gsub("  ", " ", str);
+
+    parcels <- c();
+    types <- c();
+    limcashvalues <- c();
+    landcashvalues <- c();
+    impcashvalues <- c();
+    acres <- c();
+    totalsf <- c();
+    addresses <- c();
+    sections <- c();
+    townships <- c();
+    ranges <- c();
+    ownercities <- c();
+    propertycities <- c();
+
+    landclasses <- c();
+    impclasses <- c();
+
+    ## Legal classes in Maricopa can be fractional.  This returns the
+    ## majority class of the four listed.  The problem is that it's
+    ## not always the biggest share that's listed first.
+    ## Unfortunately, as of rcs1.3, this does not work very well,
+    ## though I'm not sure why not.
+    pick.class <- function(instring) {
+
+        class <- c(); frac <- c();
+
+        class[1] <- trim(substr(instring, start=1, stop=2));
+        if (class[1] == "") return("");
+
+        frac[1] <- as.numeric(substr(instring, start=3, stop=5));
+        class[2] <- trim(substr(instring, start=6, stop=7));
+        frac[2] <- as.numeric(substr(instring, start=8, stop=10));
+        class[3] <- trim(substr(instring, start=11, stop=12));
+        frac[3] <- as.numeric(substr(instring, start=13, stop=15));
+        class[4] <- trim(substr(instring, start=16, stop=17));
+        frac[4] <- as.numeric(substr(instring, start=18, stop=20));
+
+        return(class[ which(frac == max(frac)) ]);
+    }
+
+    for (filename in c("data/maricopa/QUEE_42061CT_01.dat",
+                       "data/maricopa/MESA_42061CT_01.dat",
+                       "data/maricopa/MESA_42061CT_02.dat",
+                       "data/maricopa/CNTY_42061CT_01.dat")) {
+        ## Read the file as line images.
+        src <- read.delim(filename, head=F, sep="|", stringsAsFactors=FALSE)
+
+        for (record in src) {
+            parcels <- c(parcels, trim(substr(record, start=4, stop=12)));
+
+            types <- c(types, trim(substr(record, start=14, stop=17)));
+
+            limcashvalues <- c(limcashvalues,
+                               as.numeric(substr(record, start=432, stop=443)));
+            landcashvalues <- c(landcashvalues,
+                                as.numeric(substr(record, start=459, stop=470)));
+            impcashvalues <- c(impcashvalues,
+                               as.numeric(substr(record, start=486, stop=497)));
+            acres <- c(acres, as.numeric(substr(record, start=535, stop=546)));
+
+            rec <- substr(record, start=603, stop=617);
+            totalsf <-
+                c(totalsf,
+                  ifelse((check.numeric(rec) & !grepl("^ *$", rec)), rec, 0));
+
+
+            address <-
+                trim(trimdub(paste(trim(substr(record, start=52, stop=57)),
+                                   trim(substr(record, start=58, stop=59)),
+                                   trim(substr(record, start=60, stop=99)),
+                                   trim(substr(record, start=100, stop=111)),
+                                   trim(substr(record, start=112, stop=121)),
+                                   trim(substr(record, start=122, stop=125)),
+                                   trim(substr(record, start=126, stop=132)),
+                                   sep=" ")));
+            addresses <- c(addresses, address);
+
+            sections <- c(sections,
+                          gsub("\\\\", "0",  ## Changes \9 to 09
+                               trim(substr(record, start=27, stop=28))));
+            townships <- c(townships, trimz(substr(record, start=29, stop=31)));
+            ranges <- c(ranges, trimz(substr(record, start=32, stop=34)));
+
+            ownercities <- c(ownercities, trim(substr(record, start=378, stop=417)));
+            propertycities <- c(propertycities,
+                                trim(substr(record, start=133, stop=162)));
+
+            landclasses <- c(landclasses, trim(substr(record, start=563, stop=564)));
+            impclasses <- c(impclasses, trim(substr(record, start=583, stop=584)));
+##            landclasses <- c(landclasses, pick.class(substr(record, start=563, stop=582)));
+##            impclasses <- c(impclasses, pick.class(substr(record, start=583, stop=602)));
+        }
+    }
+
+    compress <- parcels %in% desiredparcels;
+    cat("From ", filename, ", keeping ", sum(compress), " out of ",
+        length(compress), " parcels.\n", sep="");
+    county <- rep("maricopa", sum(compress));
+
+    out <- tibble(parcel=parcels[compress],
+                  type=types[compress],
+                  impvalue=limcashvalues[compress]+impcashvalues[compress],
+                  landvalue=landcashvalues[compress],
+                  fcv=limcashvalues[compress]+impcashvalues[compress]+landcashvalues[compress],
+                  ## Data is in 1/1000 s.f. !?  convert to acres.
+                  totalsf=as.numeric(totalsf[compress]),
+                  impclass=factor(impclasses[compress],
+                                  levels=c("3", "4", "2", "1", "9", "5", "")),
+                  landclass=factor(landclasses[compress],
+                                  levels=c("3", "4", "2", "1", "9", "5", "")),
+                  area=acres[compress]/(1000 * 43560.04),
+                  address=addresses[compress],
+                  section=factor(sapply(sections[compress],
+                                        function(x) { if (nchar(x)==1)
+                                                          paste0("0",x,sep="")
+                                                      else x},
+                                        simplify=TRUE)),
+                  township=factor(townships[compress],
+                                  levels=c("2S", "3S", "1N")),
+                  range=ranges[compress],
+                  ownercity=ownercities[compress],
+                  propertycity=propertycities[compress],
+                  county=factor(county, levels=c("maricopa", "pinal"))
+                  );
+
+    newparcels <- c();
+    yearbuilts <- c();
+    poolarea <- c();
+    
+    for (filename in c("data/maricopa/QUEE_42032ct.dat",
+                       "data/maricopa/MESA_42032ct.dat",
+                       "data/maricopa/CNTY_42032ct.dat")) {
+
+        src <- read.delim(filename, head=F, sep="|", stringsAsFactors=FALSE);
+
+        for (record in src) {
+            newparcels <- c(newparcels, trim(substr(record, start=1, stop=12)));
+            yearbuilts <-
+                c(yearbuilts,
+                  as.numeric(trim(substr(record, start=42, stop=45))));
+            poolarea  <- c(poolarea, 
+                  as.numeric(trim(substr(record, start=49, stop=53))));
+        }
+    }
+
+    out <- out %>%
+        left_join(tibble(parcel=newparcels,
+                         poolarea=poolarea,
+                         pool=!is.na(poolarea),
+                         yearbuilt=yearbuilts),
+                  by="parcel")
+
+    return(out);
+}
+
+mari <- read.maricopa.data(unique(big$parcel));
+
+print(colnames(pinal));
+print(colnames(mari));
+
+prop <- rbind(pinal, mari);
+
+
