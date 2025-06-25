bbf67d45025991213ea7502445ba8c48d82419e8
tsgouros <tsgouros@gmail.com>
2025-06-25 19:40:04 -0400

    added getRates.r

---
+++ getRates.r
@@ -0,0 +1,27 @@
+
+## First get a table of account and meters with rate codes.
+rateCodes <- read.csv("data/rateData/BIF005 Rate Report.csv") %>%
+    mutate(DateMeterInstalled=mdy(DateMeterInstalled),
+           DateMeterRemoved=mdy(DateMeterRemoved)) %>%
+    as_tibble()
+
+rates <- read_excel("data/rateData/rate-fee-and-tiers.xlsx") %>%
+    mutate(baseFee=ifelse(is.na(baseFee),lag(baseFee),baseFee))
+
+applyWaterRate <- function(usage, t1, t2, t3, t4) {
+    return(ifelse(usage > 20000, (usage - 20000)*t4,0) +
+        ifelse(usage > 10000, ifelse((usage - 10000) < 10000,
+          (usage - 10000) * t3, 10000 * t3), 0) + 
+        ifelse(usage > 1000, ifelse((usage - 1000) < 9000,
+          (usage - 1000) * t2, 9000 * t2), 0) +
+        ifelse(usage > 1000, 1000 * t1, usage * t1))
+}   
+## Use like this:
+##
+##   water.means %>% ...
+##     ...
+##     left_join(rates, by = "rateCode") %>%
+##     mutate(billAmount = applyWaterRate(usage, t1, t2, t3, t4))
+##
+
+
