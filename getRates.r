f753e229a9dd92bce0f40dff172988349756cd86
tsgouros <tsgouros@gmail.com>
2025-07-01 14:21:30 -0400

    cleaning up getRates

--- getRates.r
+++ getRates.r
@@ -1,15 +1,19 @@
 
 ## First get a table of account and meters with rate codes.
 rateCodes <- read.csv("data/rateData/BIF005 Rate Report.csv") %>%
-    mutate(DateMeterInstalled=mdy(DateMeterInstalled),
-           DateMeterRemoved=mdy(DateMeterRemoved)) %>%
+    mutate(account=Account,
+           meter=Meter,
+           rateCode=RateCode,
+           dateMeterInstalled=mdy(DateMeterInstalled),
+           dateMeterRemoved=mdy(DateMeterRemoved)) %>%
+    select(-Account,-Meter,-DateMeterInstalled,-DateMeterRemoved,-RateCode) %>%
     as_tibble()
 
 rates <- read_excel("data/rateData/rate-fee-and-tiers.xlsx") %>%
     mutate(baseFee=ifelse(is.na(baseFee),lag(baseFee),baseFee))
 
-applyWaterRate <- function(usage, t1, t2, t3, t4) {
-    return(ifelse(usage > 20000, (usage - 20000)*t4,0) +
+applyWaterRate <- function(usage, baseFee, t1, t2, t3, t4) {
+    return(baseFee + ifelse(usage > 20000, (usage - 20000)*t4,0) +
         ifelse(usage > 10000, ifelse((usage - 10000) < 10000,
           (usage - 10000) * t3, 10000 * t3), 0) + 
         ifelse(usage > 1000, ifelse((usage - 1000) < 9000,
@@ -18,10 +22,13 @@ applyWaterRate <- function(usage, t1, t2, t3, t4) {
 }   
 ## Use like this:
 ##
-##   water.means %>% ...
-##     ...
-##     left_join(rates, by = "rateCode") %>%
-##     mutate(billAmount = applyWaterRate(usage, t1, t2, t3, t4))
-##
+## rev <- water.resid %>%
+##     left_join(rateCodes ,by=c("account","meter"),
+##               relationship="many-to-many") %>%
+##     left_join(rates,by="rateCode") %>%
+##     mutate(billAmount=applyWaterRate(consumption,baseFee,t1,t2,t3,t4),
+##            predBillAmount=applyWaterRate(predUsage,baseFee,t1,t2,t3,t4))
+##   
+
 
 
