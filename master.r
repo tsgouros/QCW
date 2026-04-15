646237cae3ecd137fe156fbee32e71b322c9cd92
tsgouros <tsgouros@gmail.com>
2026-04-15 11:28:37 -0400

    added TAZ

--- master.r
+++ master.r
@@ -95,7 +95,7 @@ if (assembleWaterTable) {
                   by="customer") %>%
         left_join(addressTable %>%
                   select(account, subdivision, latitude, longitude,
-                         taxProfile, parcel),
+                         taxProfile, parcel, TAZ, GRD),
                   by="account") %>%
         mutate(waterUseSegment=ifelse(service=="Irrigation",
                                       "Irrigation",
